9515ef081d96e38c0640553472094d1bad59f1c2
codetective <iaminiodu@gmail.com>
2023-09-19 14:04:40 +0100

    test no cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // const sequelize = app.get<Sequelize>(Sequelize);
   // app.enableCors();
-
   await app.listen(3000);
 }
 bootstrap();
