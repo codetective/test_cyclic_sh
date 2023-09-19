66c5937ee6fab9d740adcec894b29836a4d9b46f
codetective <iaminiodu@gmail.com>
2023-09-19 14:08:18 +0100

    test no cors

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  // const app = await NestFactory.create(AppModule, { cors: true });
   const app = await NestFactory.create(AppModule);
-  // const sequelize = app.get<Sequelize>(Sequelize);
-  // app.enableCors();
-
   await app.listen(3000);
 }
 bootstrap();
